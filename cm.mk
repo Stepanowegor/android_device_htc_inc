85385e039ebf2713842be372ed7886e907589a9d
busches <swbusche@gmail.com>
2012-05-31 08:12:06 -0500

    Fix derp when adding Torch

--- cm.mk
+++ cm.mk
@@ -13,5 +13,5 @@ PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:2.3.4/GRJ22/185897.2:user/release-keys PRIVATE_BUILD_DESC="4.08.605.2 CL185897 release-keys"
 
 # Optional CM packages
-PRODUCT_PACKAGES := \
+PRODUCT_PACKAGES += \
      Torch
