abcacf08eba3bc510503c028f872a376a2d40b60
busches <swbusche@gmail.com>
2012-08-26 15:09:04 -0500

    Update for new bootanimation

--- cm.mk
+++ cm.mk
@@ -11,6 +11,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_NAME := cm_inc
 PRODUCT_DEVICE := inc
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:4.1.1/JR003C/228551:user/release-keys PRIVATE_BUILD_DESC="inc-user 4.1.1/JR003C/228551 release-keys"
 
 # Optional CM packages
